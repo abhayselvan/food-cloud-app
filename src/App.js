d55c66ba9b5b4af57041c08e92c9ea08383e755d
Abhay Tamilselvan <abhayselvan@gmail.com>
2021-11-10 21:27:51 -0800

    amplify setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { API } from "aws-amplify";
 
 function App() {
+  async function callApi() {
+    try {
+      const restaurantData = await API.get("restaurants", "/restaurants");
+      console.log("Restaurant list", restaurantData);
+    } catch (err) {
+      console.log({ err });
+    }
+  }
+  useEffect(() => {
+    callApi();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello from foodcloud</h1>
     </div>
   );
 }
