3b0598f9e85cb9bd0e058a395c47d544d1edd0bf
Abhay Tamilselvan <abhayselvan@gmail.com>
2021-11-14 16:35:48 -0800

    added authentication and modified restaurants api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { API } from "aws-amplify";
+
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+import Amplify, { API, Auth } from "aws-amplify";
+import config from "./aws-exports";
+Amplify.configure(config);
 
 function App() {
-  async function callApi() {
-    try {
-      const restaurantData = await API.get("restaurants", "/restaurants");
-      console.log("Restaurant list", restaurantData);
-    } catch (err) {
-      console.log({ err });
-    }
-  }
+  const [userInfo, setUserInfo] = useState({});
   useEffect(() => {
-    callApi();
+    Auth.currentAuthenticatedUser({
+      bypassCache: false, // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
+    })
+      .then((user) => console.log(user.attributes))
+      .catch((err) => console.log(err));
   }, []);
   return (
     <div className="App">
       <h1>Hello from foodcloud</h1>
+      <h2>
+        <AmplifySignOut />
+      </h2>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
