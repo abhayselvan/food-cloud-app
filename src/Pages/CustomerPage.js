e0c62d07efb00b74edccf7cd5dea15dc59fcad30
Abhay Tamilselvan <abhayselvan@gmail.com>
2021-11-28 14:02:14 -0800

    customerpage

--- src/Pages/CustomerPage.js
+++ src/Pages/CustomerPage.js
@@ -1,19 +1,14 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
 import RestaurantCard from "../components/RestaurantCard";
-import OrderHistory from "./OrderHistory";
 import FlipMove from "react-flip-move";
 import Amplify, { API, Auth } from "aws-amplify";
 import config from "../aws-exports";
-import { Button } from "@mui/material";
-import { CustomerContext } from "../util/CustomerContext";
 
 Amplify.configure(config);
 
 function CustomerPage() {
-  const [page, setPage] = useState("customer");
   const [restaurants, setRestaurants] = useState([]);
-  const [userEmail, setUserEmail] = useState("");
 
   useEffect(() => {
     async function fetchData() {
@@ -28,37 +23,29 @@ function CustomerPage() {
     fetchData();
   }, []);
 
-  const handleClick = (e) => {
-    e.preventDefault();
-    setPage("orders");
-  };
-
   return (
     <div>
       <Header />
-      <CustomerContext.Provider value={{ userEmail }}>
-        {page === "customer" && (
-          <div>
-            <FlipMove>
-              {restaurants.map((restaurant) => (
-                <RestaurantCard
-                  key={restaurant.restaurantId}
-                  id={restaurant.restaurantId}
-                  name={restaurant.name}
-                  address={restaurant.address}
-                  city={restaurant.city}
-                  items={restaurant.items}
-                  cusine={restaurant.cuisine}
-                  rating={restaurant.rating}
-                  image={restaurant.imageUrl}
-                />
-              ))}
-            </FlipMove>
-            <Button onClick={handleClick}>Orders</Button>
-          </div>
-        )}
-        {page === "orders" && <OrderHistory />}
-      </CustomerContext.Provider>
+      <div>
+        <FlipMove>
+          {restaurants.map((restaurant) => (
+            <RestaurantCard
+              key={restaurant.restaurantId}
+              id={restaurant.restaurantId}
+              name={restaurant.name}
+              address={restaurant.address}
+              city={restaurant.city}
+              items={restaurant.items}
+              cusine={restaurant.cuisine}
+              rating={restaurant.rating}
+              image={restaurant.imageUrl}
+            />
+          ))}
+        </FlipMove>
+      </div>
+      <Link to="/orders">
+        <button className="Homepage_Button">Orders</button>
+      </Link>
     </div>
   );
 }
