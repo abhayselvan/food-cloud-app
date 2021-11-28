c3a0d6406156cb52c51b29871f609eae85da530d
Abhay Tamilselvan <abhayselvan@gmail.com>
2021-11-28 13:58:30 -0800

    orderhistory

--- src/Pages/OrderHistory.js
+++ src/Pages/OrderHistory.js
@@ -3,27 +3,17 @@ import Amplify, { API } from "aws-amplify";
 import config from "../aws-exports";
 import UserInfo from "../UserInfo";
 import { CustomerContext } from "../util/CustomerContext";
+import { Link } from "@reach/router";
 Amplify.configure(config);
 
 function OrderHistory() {
-  const [orders, setOrders] = useState([]);
+  const [orders, setOrders] = useState(null);
   const { userEmail } = useContext(CustomerContext);
   const { page, setPage } = useState("orders");
 
   useEffect(() => {
-    // async function fetchData() {
-    //   console.log(userEmail);
-    //   const fetchedOrders = await API.get(
-    //     "ordersapi",
-    //     "/orders/" + "arthikdk@gmail.com"
-    //   );
-    //   const data = await fetchedOrders;
-    //   console.log(data);
-    //   setOrders(data);
-    //   console.log(orders);
-    // }
-    // fetchData();
-    API.get("ordersapi", "/orders/" + "arthikdk@gmail.com")
+    console.log(userEmail);
+    API.get("ordersapi", "/orders/" + userEmail)
       .then((res) => setOrders(res))
       .catch((err) => console.log(err));
   }, []);
@@ -31,11 +21,32 @@ function OrderHistory() {
 
   return (
     <div>
-      <ul>
-        {orders.map((order, i) => {
-          return <li key={i}>{order.restaurantName}</li>;
-        })}
-      </ul>
+      {orders && (
+        <ul>
+          {orders.map((order) => {
+            return (
+              <div key={order.orderId}>
+                <h3>Order ID: {order.orderId}</h3>
+                <p>Restaurant Name: {order.restaurantName}</p>
+                <p>Creation Date: {order.creationDate}</p>
+                <p>Total Bill: {order.totalBill}</p>
+                {order.items.map((item) => {
+                  return (
+                    <div key={item.id}>
+                      <p>Item {item.id}</p>
+                      <ul>
+                        <li>Item Name: {item.name}</li>
+                        <li>Item Price: {item.price}</li>
+                        <li>Item Quantity: {item.quantity}</li>
+                      </ul>
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 }
